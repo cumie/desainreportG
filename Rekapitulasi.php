c23d1bdd06aa27726238e713bbc4db3227ba1736
Eko Wijaya <ekowijayawindows@gmail.com>
2018-01-11 19:13:38 +0800

    Mengisi data_mark1
    Belum bisa urut ke bawah

--- Rekapitulasi.php
+++ Rekapitulasi.php
@@ -38,32 +38,34 @@
   <tr>
     <td class="tg-yw4l" width="31">1.</td>
     <td class="tg-yw4l" colspan="2">Nama</td>
-    <td class="tg-yw4l" colspan="4"></td>
+        <td class="tg-yw4l" colspan="4">ARIEF NOVIARAKHMAN ZAGLADI, SE, MM</td>
+
   </tr>
   <tr>
     <td class="tg-yw4l">2.</td>
     <td class="tg-yw4l" colspan="2">Status Kepegawaian</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4"><del>PNS Dpk./</del>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTTP Yys.&nbsp/&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<del>LBS</del>&nbsp&nbsp*)</td>
   </tr>
   <tr>
     <td class="tg-yw4l">3.</td>
     <td class="tg-yw4l" colspan="2">NIK/NIDN</td>
-    <td class="tg-yw4l" colspan="4"></td>
+            <td class="tg-yw4l" colspan="4">440210001/1107118301</td>
+
   </tr>
   <tr>
     <td class="tg-yw4l">4.</td>
     <td class="tg-yw4l" colspan="2">Nomor Seri Karpeg.</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">-</td>
   </tr>
   <tr>
     <td class="tg-yw4l">5.</td>
     <td class="tg-yw4l" colspan="2">Tempat dan Tanggal Lahir</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">Banjarmasin, 7 Nopember 1983</td>
   </tr>
   <tr>
     <td class="tg-yw4l">6.</td>
     <td class="tg-yw4l" colspan="2">Jenis Kelamin</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">Laki-laki</td>
   </tr>
   <style type="text/css">
    .center  { text-align: center;}
@@ -71,37 +73,37 @@
   <tr>
     <td class="tg-yw4l">7.</td>
     <td class="tg-yw4l" colspan="2">Pendidikan Tertinggi</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">S2,Manajemen Universitas Brawijaya Malang,18 Juni 2008</td>
   </tr>
   <tr>
     <td class="tg-yw4l">8.</td>
     <td class="tg-yw4l" colspan="2">Pangkat dan Golongan Ruang / TMT</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">-</td>
   </tr>
   <tr>
     <td class="tg-yw4l">9.</td>
     <td class="tg-yw4l" colspan="2">Jabatan Fungsional / TMT</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">Asisten Ahli (100),1 September 2007</td>
   </tr>
   <tr>
     <td class="tg-yw4l">10.</td>
     <td class="tg-yw4l" colspan="2">Fakultas / Jurusan / Program Studi </td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">Manajemen</td>
   </tr>
   <tr>
     <td class="tg-yw4l" rowspan="2">11.</td>
     <td width="254" rowspan="2" class="tg-yw4l">Masa Kerja Golongan</td>
     <td width="166" class="tg-yw4l">a.Lama</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">&nbsp&nbsp&nbsp&nbsp 2 &nbsp&nbsp Tahun &nbsp&nbsp2&nbsp&nbspBulan</td>
   </tr>
   <tr>
     <td class="tg-yw4l">a.Baru</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">&nbsp&nbsp&nbsp&nbsp 5 &nbsp&nbsp Tahun &nbsp&nbsp10&nbsp&nbspBulan</td>
   </tr>
   <tr>
     <td class="tg-yw4l">12.</td>
     <td class="tg-yw4l" colspan="2">Unit Kerja</td>
-    <td class="tg-yw4l" colspan="4"></td>
+    <td class="tg-yw4l" colspan="4">STIE Panca Setia Banjarmasin</td>
   </tr>
   <tr>
     <td class="tg-baqh" rowspan="5">II</td>
@@ -114,10 +116,10 @@
   <tr>
     <td class="tg-yw4l" rowspan="3">1.</td>
     <td class="tg-yw4l" colspan="2"> UNSUR UTAMA <br/> A. PENDIDIKAN</td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
+    <td class="tg-baqh">-</td>
+    <td class="tg-baqh">50</td>
+    <td class="tg-baqh">-</td>
+    <td class="tg-baqh">50</td>
   </tr>
   <tr>
     <td class="tg-yw4l" colspan="2">B. TRIDHARMA PERGURUAN TINGGI 
@@ -125,10 +127,10 @@
     <br/> b. Melaksanakan Penelitian
     <br/> c. Melaksanakan Pengabdian Kepada Masyarakat
     </td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
+    <td class="tg-baqh">83.02</td>
+    <td class="tg-baqh">101.50</td>
+    <td class="tg-baqh">-</td>
+    <td class="tg-baqh">184.52</td>
   </tr>
   <tr>
     <td class="tg-baqh" colspan="2">JUMLAH</td>
@@ -139,28 +141,30 @@
   </tr>
   <tr>
     <td class="tg-yw4l">2.</td>
-    <td class="tg-yw4l" colspan="2">UNSUR PENUNJANG <br/> Melaksanakan Penunjang Tugas Pokok Dosen</td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
+    <td class="tg-yw4l" colspan="2">UNSUR PENUNJANG 
+      <br/> Melaksanakan Penunjang Tugas Pokok Dosen
+    <td class="tg-baqh">1.51</td>
+    <td class="tg-baqh">11</td>
+    <td class="tg-baqh">-</td>
+    <td class="tg-baqh">12.51</td>
+
   </tr>
   <tr>
     <td class="tg-baqh" colspan="4">JUMLAH UNSUR UTAMA DAN UNSUR PENUNJANG</td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
-    <td class="tg-baqh"></td>
+    <td class="tg-baqh">100</td>
+    <td class="tg-baqh">215.50</td>
+    <td class="tg-baqh">-</td>
+    <td class="tg-baqh">315.50</td>
   </tr>
   <tr>
     <td class="tg-baqh">III</td>
      <td class="tg-yw4l" colspan="5">DAPAT DIANGKAT DALAM JABATAN: &nbsp&nbsp&nbsp&nbsp&nbsp Lektor(200)   
       <br/>Dalam Mata Kuliah : <ol>
- <li>Riset Operasional</li>
+        <li>Riset Operasional</li>
             <li>Manajemen Strategi</li>
             <li>Manajemen Operasional</li>
           </ol>
-       <td class="tg-yw4l" colspan="2">TMT.: 01 MEI 2016 
+       <td class="tg-yw4l" colspan="2"TMT.: 01 MEI 2011 
          </td>
 </table>
 
