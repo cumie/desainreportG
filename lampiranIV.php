bf3aed4ad1d24de9ae101bfd8d8fd3ad8eae9850
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-11 23:00:35 +0800

    percobaan membuat struktur tabel

--- lampiranIV.php
+++ lampiranIV.php
@@ -1,2 +1,17 @@
 <?php include('head.php');
 ?>
+
+<table class="tg">
+
+  <tr>
+  
+  	<th></th>
+		<th></th>
+		<th></th>
+		<th></th>
+		<th></th>
+    <th></th>
+    <th></th>
+  </tr>
+
+</table>
