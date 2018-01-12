152c45b67c593a0adb37a3972cfbde05e6b82e31
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-13 02:47:59 +0800

    menambahkan baris tabel (jumlah)

--- lampiranIV.php
+++ lampiranIV.php
@@ -3,15 +3,19 @@
 
 
 <table class="tg" width="100%" frame="border">
+
+
   <tr>
    <th  class="tg-baqh" width="15"><b><center>NO.</center></b></th>
-  	<th class="tq-baqh" width="100"><b><center>KEGIATAN PENGABDIAN <BR/> PADA MASYARAKAT</center></b></th>
-  	<th class="tq-baqh" width="100"><b><center>BENTUK</center></b></th>
-	<th class="tq-baqh" width="150"><b><center>TEMPAT  / <br/> INSTANSI</center></b></th>
-	<th class="tq-baqh" width="100"><b><center>TANGGAL</center></b></th>
-	<th class="tq-baqh" width="100"><b><center>JML. ANGKA <br/> KREDIT</center></b></th>	
-	<th class="tq-baqh" width="180"><b><center>KETERANGAN</center></b></th>	
+  	<th class="tg-baqh" width="100"><b><center>KEGIATAN PENGABDIAN <BR/> PADA MASYARAKAT</center></b></th>
+  	<th class="tg-baqh" width="100"><b><center>BENTUK</center></b></th>
+	<th class="tg-baqh" width="150"><b><center>TEMPAT  / <br/> INSTANSI</center></b></th>
+	<th class="tg-baqh" width="100"><b><center>TANGGAL</center></b></th>
+	<th class="tg-baqh" width="100"><b><center>JML. ANGKA <br/> KREDIT</center></b></th>	
+	<th class="tg-baqh" width="180"><b><center>KETERANGAN</center></b></th>	
   </tr>
+  
+  
 <tr> 
   	<th><center><b>1</b></center></th>
 		<th><center><b>2</b></center></th>
@@ -19,9 +23,10 @@
 		<th><center><b>4</b></center></th>
 		<th><center><b>5</b></center></th>
 		<th><center><b>6</b></center></th>
-    <th><center><b>7</b></center></th>
-    
+    <th><center><b>7</b></center></th>  
   </tr>
+  
+  
   <tr class="tg">
 		<td class="tg-baqh"></td>
 		<td class="tg-baqh" width="250" >
@@ -33,6 +38,8 @@
 		<td> </td>
     <td> </td>
 	</tr>
+	
+	
 	<tr class="tg" >
 		<td class="tg-baqh"><p style="font-size: 16">1.</td></p>
 		<td class="tg-baqh">
@@ -47,8 +54,9 @@
 		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
   	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
     <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
-	
 	</tr>
+	
+	
 	<tr class="tg" >
 		<td class="tg-baqh"><p style="font-size: 16">2.</td></p>
 		<td class="tg-baqh">
@@ -63,8 +71,9 @@
 		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
   	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
     <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
+	</tr>
 	
-	</tr><tr class="tg" >
+	<tr class="tg" >
 		<td class="tg-baqh"><p style="font-size: 16">3.</td></p>
 		<td class="tg-baqh">
 			<p style="font-size: 16"> Memberi Penyuluhan<br/> Mengenai Manajemen<br/>
@@ -78,7 +87,19 @@
 		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
   	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
     <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
+	</tr>
 	
+	
+	<tr class="tg">
+		<td class="tg-baqh bold"></td>
+		<td>
+			<b><center><p style="font-size: 16"> Jumlah </center></b></p>
+		</td>
+		<td </td>
+		<td class="tg-yw4l"></td>
+		<td class="tg-yw4l"></td>
+  	<td class="tg-baqh"><b><p style="font-size: 16">3</b></td></p>
+    <td class="tg-yw4l"></td>
 	</tr>
 
 </table>
