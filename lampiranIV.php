5a3c4cd585c86c64e7375890f538a0ce87c519d8
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-13 02:41:41 +0800

    membuat tabel dan mengisinya

--- lampiranIV.php
+++ lampiranIV.php
@@ -4,14 +4,81 @@
 
 <table class="tg" width="100%" frame="border">
   <tr>
-   <th  class="tg-bagh" width="15"><b><center>NO.</center></b></th>
-  	<th class="tq-bagh" width="250"><b><center>KEGIATAN PENGABDIAN <BR/> PADA MASYARAKAT</center></b></th>
-  	<th class="tq-bagh" width="100"><b><center>BENTUK</center></b></th>
-	<th class="tq-bagh" width="150"><b><center>TEMPAT  / <br/> INSTANSI</center></b></th>
-	<th class="tq-bagh" width="100"><b><center>TANGGAL</center></b></th>
-	<th class="tq-bagh" width="100"><b><center>JML. ANGKA <br/> KREDIT</center></b></th>	
-	<th class="tq-bagh" width="180"><b><center>KETERANGAN</center></b></th>	
+   <th  class="tg-baqh" width="15"><b><center>NO.</center></b></th>
+  	<th class="tq-baqh" width="100"><b><center>KEGIATAN PENGABDIAN <BR/> PADA MASYARAKAT</center></b></th>
+  	<th class="tq-baqh" width="100"><b><center>BENTUK</center></b></th>
+	<th class="tq-baqh" width="150"><b><center>TEMPAT  / <br/> INSTANSI</center></b></th>
+	<th class="tq-baqh" width="100"><b><center>TANGGAL</center></b></th>
+	<th class="tq-baqh" width="100"><b><center>JML. ANGKA <br/> KREDIT</center></b></th>	
+	<th class="tq-baqh" width="180"><b><center>KETERANGAN</center></b></th>	
   </tr>
-
+<tr> 
+  	<th><center><b>1</b></center></th>
+		<th><center><b>2</b></center></th>
+		<th><center><b>3</b></center></th>
+		<th><center><b>4</b></center></th>
+		<th><center><b>5</b></center></th>
+		<th><center><b>6</b></center></th>
+    <th><center><b>7</b></center></th>
+    
+  </tr>
+  <tr class="tg">
+		<td class="tg-baqh"></td>
+		<td class="tg-baqh" width="250" >
+			<p style="font-size: 16"><b><u>Memberi Penyuluhan Kepada<br/> Masyarakat</u></b></p>
+		</td>
+		<td> </td>
+		<td> </td>
+		<td> </td>
+		<td> </td>
+    <td> </td>
+	</tr>
+	<tr class="tg" >
+		<td class="tg-baqh"><p style="font-size: 16">1.</td></p>
+		<td class="tg-baqh">
+			<p style="font-size: 16"> Memberi Penyuluhan<br/> Mengenai Manajemen<br/>
+			Pemasaran Pada program<br/> Pelatihan Kerja<br/>
+			Berbasis Masyarakat<br/> Melalui Terapan<br/>
+			Teknologi Tepat Guna<br/>
+			</p>
+		</td>
+		<td class="tg-baqh" align="center"><p style="font-size: 16">Penyuluhan</td> </p>
+		<td class="tg-yw4l"><p style="font-size: 16">Desa Tambak Danau<br/> Kec. Astambul<br/> Kab. Banjar Kalsel</td></p>
+		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
+  	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
+    <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
 	
+	</tr>
+	<tr class="tg" >
+		<td class="tg-baqh"><p style="font-size: 16">2.</td></p>
+		<td class="tg-baqh">
+			<p style="font-size: 16"> Memberi Penyuluhan<br/> Mengenai Manajemen<br/>
+			Pemasaran Pada program<br/> Pelatihan Kerja<br/>
+			Berbasis Masyarakat<br/> Melalui Terapan<br/>
+			Teknologi Tepat Guna<br/>
+			</p>
+		</td>
+		<td class="tg-baqh" align="center"><p style="font-size: 16">Penyuluhan</td> </p>
+		<td class="tg-yw4l"><p style="font-size: 16">Desa Tambak Danau<br/> Kec. Astambul<br/> Kab. Banjar Kalsel</td></p>
+		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
+  	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
+    <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
+	
+	</tr><tr class="tg" >
+		<td class="tg-baqh"><p style="font-size: 16">3.</td></p>
+		<td class="tg-baqh">
+			<p style="font-size: 16"> Memberi Penyuluhan<br/> Mengenai Manajemen<br/>
+			Pemasaran Pada program<br/> Pelatihan Kerja<br/>
+			Berbasis Masyarakat<br/> Melalui Terapan<br/>
+			Teknologi Tepat Guna<br/>
+			</p>
+		</td>
+		<td class="tg-baqh" align="center"><p style="font-size: 16">Penyuluhan</td> </p>
+		<td class="tg-yw4l"><p style="font-size: 16">Desa Tambak Danau<br/> Kec. Astambul<br/> Kab. Banjar Kalsel</td></p>
+		<td class="tg-yw4l"><p style="font-size: 16">28 Oktober 2010</td></p>
+  	<td class="tg-baqh"><p style="font-size: 16">1</td></p>
+    <td class="tg-yw4l"><p style="font-size: 16">Laporan Terlampir</td></p>
+	
+	</tr>
+
 </table>
