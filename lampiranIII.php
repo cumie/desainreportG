c712eed9fc6a33b9358300148e2c016302cf3f1f
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-01-13 16:12:14 +0800

    membuat table dosen yang dinilai

--- lampiranIII.php
+++ lampiranIII.php
@@ -1,4 +1,5 @@
-<table class="">
+<link rel="stylesheet" type="text/css" href="css/site.css">
+<table class="goku">
 	<tr>
 		<th>Lampiran</th>
 		<th>KEPUTUSAN BERSAMA MENTERI</th>
@@ -25,4 +26,33 @@
 	</tr>
 
 </table>
-<h2>DAFTAR KEGIATAN PENELITIAN</h2>
+<h2 class="kendil">DAFTAR KEGIATAN PENELITIAN</h2>
+
+<table class="goku2">
+	<tr>
+		<th colspan="2">Dosen yang Dinilai</th>
+		<th>:</th>
+		<th></th>
+	</tr>
+	</br>
+	<tr>
+		<th colspan="2">1. Nama</th>
+		<th>:</th>
+		<th>ARIEF NOVIARAKHMAN ZAGLADI, SE, MM</th>
+	</tr>
+	<tr>
+		<th colspan="2">2. NIP/NIK</th>
+		<th>:</th>
+		<th>1107118301</th>
+	</tr>
+	<tr>
+		<th colspan="2">3. Jabatan Fungsional</th>
+		<th>:</th>
+		<th>Asisten Ahli(100),1 September 2007</th>
+	</tr>
+	<tr>
+		<th colspan="2">4. Unit Kerja</th>
+		<th>:</th>
+		<th>STEI Panca Setia Banjarmasin</th>
+	</tr>
+</table>
