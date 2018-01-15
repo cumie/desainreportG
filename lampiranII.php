c1ac11a7061eb7aeb20195d7c9658c510dac09c7
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-01-15 23:12:08 +0700

    Buat LampranII

---
+++ lampiranII.php
@@ -0,0 +1,15 @@
+-<?php include('head2.php');
+?>
+
+<table class="tisi">
+  <tr>
+  	<th rowspan="2">NO.</th>
+		<th rowspan="2" width="400">KEGIATAN PENDIDIKAN DAN PENGAJARAN</th>
+  </tr>
+	<tr>
+		<th>TEMPAT / INSTANSI</th>
+		<th width="200">TANGGAL</th>
+		<th width="150">JUMLAH ANGKA KREDIT</th>
+    <th width="150">KETERANGAN / BUKTI FISIK</th>
+  </tr>
+  
