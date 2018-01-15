1d77e75520b941569c7e534bc8e75c8c03dd9e9a
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-01-16 02:37:20 +0800

    Menambah link lampiran II

--- head.php
+++ head.php
@@ -22,8 +22,8 @@ include ('library.php');
 				<li><a href="./">Beranda</a></li> 
 				<li><a href="test_cetak.php" target="_blank">Contoh Report</a></li> 
 				<li><a href="Rekapitulasi.php">Rekapitulasi</a></li>
-				 			<li><a href="./">Lampiran I</a></li>
-				<li><a href="./">Lampiran II</a></li> 
+				<li><a href="./">Lampiran I</a></li>
+				<li><a href="lampiranII.php">Lampiran II</a></li> 
 				<li><a href="LampiranIII.php">Lampiran III</a></li> 
 				<li><a href="LampiranIV.php">Lampiran IV</a></li> 
 				<li><a href="./">Lampiran V</a></li> 
