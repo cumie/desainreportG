947e0daaf94f5ecaa7bae5bd1ebca32ec4b54ff7
Khairul Ikhsan Nurzeha <32305997+ikhsannz@users.noreply.github.com>
2018-01-25 10:56:34 +0800

    penambahan bagian atas dan bawah, by : abiem adillah arifin

--- lampiranIV.php
+++ lampiranIV.php
@@ -1,6 +1,143 @@
 <?php include('head.php');
 ?>
+<link rel="stylesheet" type="text/css" href="css/site.css">
+<table class="ab">
 
+<link href="css/site.css" rel="stylesheet" type="text/css" /> 
+<style type="text/css">
+
+</style>
+<style type="text/css">
+   .left    { text-align: left;}
+   .right   { text-align: right;}
+   .center  { text-align: center;}
+   .justify { text-align: justify;}
+</style>
+<table width="380" class="ab">
+	<tr>
+		<th width="120" colspan="1">Lampiran IV</th> 
+		<th width="300">KEPUTUSAN BERSAMA MENTERI</th>
+	</tr>
+	<tr>
+		<th></th>
+		<th>PENDIDIKAN DAN KEBUDAYAAN DAN</th>
+	</tr>
+	<tr>
+		<th></th>
+		<th>KEPALA BADAN KEPEGAWAIAN NEGARA</th>
+	</tr>
+	<tr>
+		<th></th>
+		<th>NOMOR   :	61409/MPK/KP/99</th>
+	</tr>
+	<tr>
+		<th></th>
+		<th>NOMOR   : 181 TAHUN 1999</th>
+	</tr>
+	<tr>
+		<th></th>
+		<th>TANGGAL : 13 OCTOBER 1999</th>
+	</tr>
+</table>
+<tr>
+<h2 class="abc">SURAT PERNYATAAN</h2>
+</tr>
+<tr>
+<h2 class="abcd">MELAKSANAKAN KEGIATAN PENGABDIAN PADA MASYARAKAT</h2>
+</tr>
+
+<table width="643" class="abcde">
+	<tr>
+		<th colspan="9">Yang bertandatangan dibawah ini </th>
+		<th width="9">&nbsp;</th>
+		<th width="14">:</th>
+		<th width="20">&nbsp;</th>
+		<th width="393"></th>
+	</tr>
+	<tr>
+		<th colspan="9">Nama </th>
+		<th width="9">&nbsp;</th>
+		<th width="14">:</th>
+		<th width="20">&nbsp;</th>
+		<th width="393">SUTRISNO, SE, MM</th>
+	</tr>
+	<tr>
+		<th colspan="9">NIP/NIK</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>19670512 200501 1 001</th>
+	</tr>
+	<tr>
+		<th colspan="9">Pangkat / Golongan Ruang</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>Penata, III/c</th>
+	</tr>
+	
+	<tr>
+		<th colspan="9">Jabatan Fungsional</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>Lektor (300)</th>
+	</tr>
+	<tr>
+		<th colspan="9">Unit Kerja</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>STEI Panca Setia Banjarmasin</th>
+	</tr>
+</table>
+</br>
+
+<table width="643" class="abcde">
+	<tr>
+		<th colspan="9">Menyatakan Bahwa </th>
+		<th width="9">&nbsp;</th>
+		<th width="14">:</th>
+		<th width="20">&nbsp;</th>
+		<th width="393"></th>
+	</tr>
+	<tr>
+		<th colspan="9">Nama </th>
+		<th width="9">&nbsp;</th>
+		<th width="14">:</th>
+		<th width="20">&nbsp;</th>
+		<th width="393">ARIEF NOVIARAKHMAN ZAGLADI, SE, MM</th>
+	</tr>
+	<tr>
+		<th colspan="9">NIP/NIK</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>1107118301</th>
+	</tr>
+	<tr>
+		<th colspan="9">Pangkat / Golongan Ruang</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>-</th>
+	</tr>
+	<tr>
+		<th colspan="9">Jabatan Fungsional</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>Asisten Ahli(100),1 September 2007</th>
+	</tr>
+	<tr>
+		<th colspan="9">Unit Kerja</th>
+		<th>&nbsp;</th>
+		<th>:</th>
+		<th>&nbsp;</th>
+		<th>STEI Panca Setia Banjarmasin</th>
+	</tr>
+</table>
+</br>
 
 <table class="tg" width="100%" frame="border">
 
@@ -17,13 +154,13 @@
   
   
 <tr> 
-  	<th><center><b>1</b></center></th>
+		<th><center><b>1</b></center></th>
 		<th><center><b>2</b></center></th>
 		<th><center><b>3</b></center></th>
 		<th><center><b>4</b></center></th>
 		<th><center><b>5</b></center></th>
 		<th><center><b>6</b></center></th>
-    <th><center><b>7</b></center></th>  
+		<th><center><b>7</b></center></th>  
   </tr>
   
   
@@ -94,7 +231,7 @@
 	<tr class="tg">
 		<td class="tg-baqh bold"></td>
 		<td>
-			<b><center><p style="font-size: 16"> Jumlah </center></b></p>
+			<b><center><p style="font-size: 16"> Jumlah </p></center></b>
 		</td>
 		<td </td>
 		<td class="tg-yw4l"></td>
@@ -102,5 +239,37 @@
   	<td class="tg-baqh"><b><p style="font-size: 16">3</b></td></p>
     <td class="tg-yw4l"></td>
 	</tr>
-
 </table>
+
+<table  width="100%"  >
+<tr class="tg-baqh">
+	<p><p style="font-size: 14" align="left">Demikian pernyataan ini dibuat untuk dapat dipergunakan sebagaimana mestinya.</td>
+	<p style="font-size: 14" align="center"></td></p>
+	<p style="font-size: 14" align="right">Banjarmasin, 30 April 2011<br/>
+	<br/>
+	Yang Membuat Pernyataan<br/>
+	Puket I STIE Panca Setia B.Masin<br/>
+	<br/>
+	<br/>
+	</p></td>	
+</tr>
+
+<tr class="tg-baqh">
+	<td <p style="font-size: 14" align="left"><p>Catatan : 
+	<p style="text-align: left">1. Dibuat per semester.
+	<p>2. Ditanda tangani oleh Ketua Jurusan pada Univ/Sekolah TInggi/Akedemi/Politeknik.</p>
+	3. Dilampirkan bukti-bukti kegiatan tersebut di atas.</td>
+	<td class="tg-baqh" width="100"><p style="font-size: 12" align="center"></td></p>
+	<td class="tg-baqh" width="250" align="right"><p style="font-size: 14" align="right"><br/>
+	<br/>
+	<br/>
+	Sutrisno, SE, MM<br/>
+	NIP. 19670512 200501 1 001<br/>
+	
+	</p></td>	
+</tr>
+</table>
+
+<?php include('foot.php');
+
+?>
