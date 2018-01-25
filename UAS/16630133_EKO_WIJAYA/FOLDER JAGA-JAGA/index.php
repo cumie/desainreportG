fba39c889466acd1ee2f6267d0b58caa7fc6e814
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-01-25 22:28:35 +0800

    testing

---
+++ UAS/16630133_EKO_WIJAYA/FOLDER JAGA-JAGA/index.php
@@ -0,0 +1,29 @@
+<?php include "head.php"; ?>
+
+        
+
+	<div class="col-sm-9">
+		<p><center>
+		
+			<p><center><h1> WELCOME TO WEB CRUD </h1>
+
+		<hr/>
+		
+		<div class="well"> 
+		
+			
+			
+
+		</div>
+	</div>
+	
+		
+		
+		<div class="sidebar">
+	
+			
+		</div>	
+		
+	
+
+<?php include "foot.php"; ?>    
