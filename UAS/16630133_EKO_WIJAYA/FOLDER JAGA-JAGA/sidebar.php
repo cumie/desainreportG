3099365687a372143ea1f571b1f9534dc4645e97
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-01-25 22:26:26 +0800

    hapus

--- UAS/16630133_EKO_WIJAYA/FOLDER JAGA-JAGA/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="col-sm-2">
-			<div class="sidebar"> 
-			<h2> Latihan Sidebar </h2>
-			<hr/>
-			<div class="well">
-			<img class="img" src="img/shobah.jpg" width="60%">
-			</div>	
-			</div>	
-		</div>	
