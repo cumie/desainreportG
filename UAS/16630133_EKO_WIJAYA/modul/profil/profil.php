6f9ad8c6f1c152853418cfe3473b528b575c29d9
Eko Wijaya <ekowijayawindows@gmail.com>
2018-01-27 01:03:02 +0800

    Testing

---
+++ UAS/16630133_EKO_WIJAYA/modul/profil/profil.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+<!--
+.style1 {font-family: Arial, Helvetica, sans-serif}
+-->
+</style>
+<h2 class='head' align="Center">W17AYA</h2>
+<div align="center"><img src="material/images/icon.jpg" width="20%" height="37%"/></div>
+<p align="justify" class="style1">	</p> 
+
+<p align="justify">&nbsp;</p>
+
+
