7dd29b219862b160a0627f380e1c61fed606953f
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-01-26 16:37:04 +0800

    dihapus dlu

--- UAS/16630305_ESTI_YULIANI/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="col-sm-3">
-			<div class="sidebar"> 
-			<h2>  </h2>
-			<hr/>
-			<div class="well">
-			<img class="img" src="img/thumb.jpg" width="60%">
-			</div>	
-			</div>	
-		</div>	
